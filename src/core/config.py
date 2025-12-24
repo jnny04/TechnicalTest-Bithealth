889545f2e3fbbb09915609b84243bd0f7d6e2215
jny <162534685+jnny04@users.noreply.github.com>
2025-12-24 18:06:17 +0700

    Delete src directory

--- src/core/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-class Settings:
-    APP_TITLE: str = "Learning RAG Demo - BitHealth Refactor"
-    QDRANT_URL: str = os.getenv("QDRANT_URL", "http://localhost:6333")
-    COLLECTION_NAME: str = "demo_collection"
-    VECTOR_SIZE: int = 128
-
-settings = Settings()
